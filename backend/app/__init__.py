3442a9897cd4ce933286d31d41fccdb1e4d7d83d
joon616 <21f1004357@ds.study.iitm.ac.in>
2025-02-25 18:25:58 +0530

    API setup and add Chats resource

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from .models import db
+from .resources import api
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
     db.init_app(app)
+    api.init_app(app)
 
     return app
