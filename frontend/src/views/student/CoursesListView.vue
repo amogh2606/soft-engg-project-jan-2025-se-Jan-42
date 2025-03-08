0a2ea222d6141af9a3e3e9c1076b679695d46167
Mayank <ay160920@gmail.com>
2025-03-08 19:40:13 +0530

    Implemented course enrollment for user

--- frontend/src/views/student/CoursesListView.vue
+++ frontend/src/views/student/CoursesListView.vue
@@ -1,68 +1,131 @@
 <script setup>
+import { enrollUserInCourse, getAllCourses, getUser } from '@/api';
+import EyeIcon from '@/components/icons/EyeIcon.vue';
+import PlusIcon from '@/components/icons/PlusIcon.vue';
 import Button from '@/components/ui/buttons/Button.vue';
+import TableComponent from '@/components/ui/table/TableComponent.vue';
 import { useAuthStore } from '@/stores/auth';
-import { onMounted, ref } from 'vue';
+import { push } from 'notivue';
+import { computed, onMounted, ref } from 'vue';
+import { RouterLink } from 'vue-router';
 import BaseView from './BaseView.vue';
+
+const { user, setUser } = useAuthStore();
+const activeTab = ref('enrolled');
+const headers = ref([
+    { label: 'Course Name', key: 'name' },
+    { label: 'Actions', key: 'actions' },
+]);
 const courses = ref([]);
+const enrolledCourses = ref([]);
+const filteredCourses = computed(() => {
+    if (activeTab.value === 'enrolled') {
+        return enrolledCourses.value;
+    }
+    return courses.value.filter((c) => !enrolledCourses.value.find((ec) => ec.id === c.id));
+});
+
+const fetchCourses = () => {
+    getUser()
+        .then((response) => {
+            enrolledCourses.value = response.data.courses;
+            setUser(response.data);
+        })
+        .catch((error) => {
+            console.error(error);
+            push.error('Error fetching courses !');
+        });
+    getAllCourses()
+        .then((response) => {
+            courses.value = response.data;
+        })
+        .catch((error) => {
+            console.error(error);
+            push.error('Error fetching courses !');
+        });
+};
 
 onMounted(() => {
-    const { userCourses } = useAuthStore();
-    courses.value = userCourses;
+    fetchCourses();
 });
+
+const enrollInCourse = (userId, courseId) => {
+    enrollUserInCourse(userId, courseId)
+        .then((res) => {
+            push.success('Course enrolled successfully');
+            fetchCourses();
+        })
+        .catch((err) => {
+            console.error(err);
+            push.error('Failed to enroll in course');
+        });
+};
 </script>
 <template>
     <BaseView>
         <template #main-slot>
-            <div class="flex flex-1 flex-col justify-center overflow-hidden pt-4">
+            <div class="flex flex-1 flex-col overflow-hidden pt-4">
                 <div
                     class="mx-2 mb-2 flex flex-col overflow-hidden rounded-lg border bg-white p-4 shadow md:mx-10 md:mb-10"
                 >
                     <h1 class="p-3 pb-7 text-center text-2xl font-semibold md:mx-10 md:px-8">
-                        Enrolled Courses
+                        Student Courses
                     </h1>
-                    <div v-if="courses.length === 0" class="text-center text-xl text-gray-600">
-                        No courses available !
-                    </div>
-                    <div
-                        v-else
-                        class="custom-grid w-full overflow-y-scroll rounded-lg border px-1 py-4 shadow"
-                    >
-                        <div
-                            v-for="course in courses"
-                            :key="course.id"
-                            class="custom flex aspect-square w-52 flex-col items-center justify-between rounded-lg border border-gray-300 text-center shadow hover:bg-gray-300 hover:shadow-md"
-                        >
-                            <p class="my-auto px-2 text-xl font-semibold text-white">
-                                {{ course.name }}
-                            </p>
 
-                            <RouterLink :to="'/course/' + course.id" class="w-full">
-                                <Button class="mt-2 w-full rounded-b-lg" varient="light"
-                                    >View Course</Button
-                                >
-                            </RouterLink>
+                    <div class="flex flex-col overflow-hidden rounded-lg border bg-white shadow">
+                        <div class="flex border-b">
+                            <button
+                                class="px-6 py-3 font-medium"
+                                :class="{
+                                    'border-b-2 border-blue-500 text-blue-500':
+                                        activeTab === 'enrolled',
+                                }"
+                                @click="activeTab = 'enrolled'"
+                            >
+                                Enrolled Courses
+                            </button>
+                            <button
+                                class="px-6 py-3 font-medium"
+                                :class="{
+                                    'border-b-2 border-blue-500 text-blue-500': activeTab === 'all',
+                                }"
+                                @click="activeTab = 'all'"
+                            >
+                                Available Courses
+                            </button>
                         </div>
+                        <div class="flex justify-between gap-2 border-b p-3">
+                            <input
+                                type="text"
+                                class="w-full rounded border p-2"
+                                placeholder="Search..."
+                            />
+                            <Button varient="primary">Search</Button>
+                        </div>
+
+                        <TableComponent :headers="headers" :rows="filteredCourses">
+                            <template #actions="{ row }">
+                                <RouterLink
+                                    v-if="enrolledCourses?.find((c) => c.id === row.id)"
+                                    :to="`/course/${row.id}`"
+                                >
+                                    <Button varient="light" :rounded="true">
+                                        <EyeIcon class="h-6 w-6" />
+                                    </Button>
+                                </RouterLink>
+                                <Button
+                                    v-else
+                                    varient="light"
+                                    :rounded="true"
+                                    @click="enrollInCourse(user.id, row.id)"
+                                >
+                                    <PlusIcon class="h-6 w-6" />
+                                </Button>
+                            </template>
+                        </TableComponent>
                     </div>
                 </div>
             </div>
         </template>
     </BaseView>
 </template>
-
-<style scoped>
-.custom {
-    background-image: url('../../assets/images/course-thumbnail.svg');
-    background-color: #e7e7ef;
-    opacity: 0.8;
-    background-image:
-        repeating-radial-gradient(circle at 0 0, transparent 0, #e7e7ef 10px),
-        repeating-linear-gradient(#444cf755, #444cf7);
-    background-blend-mode: multiply;
-}
-.custom-grid {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(min(200px, 100%), 1fr));
-    place-items: center;
-    gap: 1rem;
-}
-</style>
